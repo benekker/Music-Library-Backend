bceba1dbd9d005bcf7f5fedb74ea1fdbdaec00e3
benekker <benekker1@gmail.com>
2022-08-05 13:32:34 -0500

    Changed database settings + created Songs app

--- Music_Library_Project/settings.py
+++ Music_Library_Project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6ttki#q%0+z&dds$_9j++nf*3%v_3rqj1on45cgxkjmw6+9i0^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,17 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Music_Library_Project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -123,5 +110,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from Music_Library_Project.local_settings import *
+except ImportError:
+    pass
 
 CORS_ORIGIN_ALLOW_ALL=True
