332805f19e545fec4db7a89dac56944411418df1
benekker <benekker1@gmail.com>
2022-08-05 14:04:53 -0500

    Created and modified url paths + Created variable constants file + Created Get and Post functionality in views file

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Song
+from .serializers import SongSerializer
+from .variable_constants import GET, POST, PUT, DELETE
 
 # Create your views here.
+
+@api_view([GET, POST])
+def song_list(request):
+
+    if request.method == GET:
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == POST:
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
